8515bd6c2e137b3a124333b8391b1f91bd02a79e
Vaskin1KTVR <nvvm@bk.ru>
2016-01-15 10:03:48 +0200

    last

--- JavaScript/urok1/js/novosti.js
+++ JavaScript/urok1/js/novosti.js
@@ -1,13 +1,5 @@
-/*if email="":
-	alert("Vvedite email!");
-
-if pwd="";
-	alert("Vvedite parol!");*/
-
 'use strict';
-
-
-
+//alert("Hello, World!");
 function valid(){
 	var title = document.getElementById("title");
 	var theme = document.getElementById("theme");
@@ -15,32 +7,28 @@ function valid(){
 	var content = document.getElementById("content");
 	var date = document.getElementById("date");
 
-	if(title.value ==""){
-		alert("You have not entered the title!");
+	if(title.value == ""){
+		alert("You have not entered the title line!");
 		return false;
 	}
 	if(theme.value == ""){
-		alert("You have not entered the theme!");
+		alert("You have not entered the theme line!");
 		return false;
 	}
 	if(author.value == ""){
-		alert("You have not entered the author!");
+		alert("You have not entered the author line!");
 		return false;
 	}
 	if(content.value == ""){
-		alert("You have not entered the content!");
+		alert("You have not entered the content line!");
 		return false;
 	}
-	if(date.value == ""){
-		alert("You have not entered the date!");
-		return false;
-	}
-	var d = new Date();
-	//document.getElementById("demo").innerHTML = d.toDateString();
-	document.getElementById("date").innerHTML = d.getDate()+"."+d.getMonth()+d.getFullYear()+" / "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
-
-
+		
 	alert("Hurray, You have published it!");
 	return true;
 
 }
+var d = new Date();
+	//document.getElementById("date").innerHTML = d.toDateString();
+	document.getElementById("date").innerHTML = d.getDate()+"."+d.getMonth()+"."+d.getFullYear()+" // "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+
